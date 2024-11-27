72cbc64a478f6316eccfb7ee48050ed3394dadd0
R1cky07 <156109040+R1cky07@users.noreply.github.com>
2024-11-27 16:27:13 +0100

    Added the option for end the program
    The options were 6 and now are 7 because you can end the program at the start

--- project.cpp
+++ project.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-// #include "bank.h"
+// #include "Client.h"
+// #include "Client.cpp"
 using namespace std;
 
 void start(int choice = 0);
@@ -7,14 +8,17 @@ bool check(int firstChoice, int secondChoice = 0);
 
 int main()
 {
-    start();                 // low duration = 2 month, medium = 6 month, long = 12 month
+    start(); // low duration = 2 month, medium = 6 month, long = 12 month
     cout << "Your choice: "; //per l'investimento usare rand()%200 - 100 in modo da capire la percentuale casuale del quale aumenta
     int choice[2];
     cin >> choice[0];
-    while(choice[0] < 1 || choice[0] > 6) {
-        cout << "Select a parameter between 1 and 6" << endl;
+    while(choice[0] < 1 || choice[0] > 7) {
+        cout << "Select a parameter between 1 and 7" << endl;
         cout << "Your choice: ";
         cin >> choice[0];
+    }
+    if(choice[0] == 7) {
+        return 0;
     }
     if(choice[0] <= 3) {
         start(choice[0]);
@@ -27,19 +31,10 @@ int main()
     }
 
     if(choice[0] == 4) {
-        cout << "How long do you want to advance in time" << endl;
-        cout << "days(1), months(2), years(3): ";
-        int timeChoice;
-        cin >> timeChoice;
-        while(timeChoice < 1 || timeChoice > 3) {
-            cout << "Invalid choice" << endl;
-            cout << "Reinsert: ";
-            cin >> timeChoice;
-        }
-        cout << "Now insert the total time: ";
+        cout << "How long do you want to advance in time (the time is in month): ";
         int time;
         cin >> time;
-		//client.setTime(time);
+        // client.setTime(time);
     }
 
     /*if(choice[0] == 5){
@@ -61,7 +56,8 @@ void start(int choice)
         cout << "3 - Invest money with different margins of risk or profit" << endl;
         cout << "4 - Advance over time" << endl;
         cout << "5 - See your bank status" << endl;
-        cout << "6 - See your non-deposited wallet" << endl << endl;
+        cout << "6 - See your non-deposited wallet" << endl;
+        cout << "7 - Stop program" << endl << endl;
         return;
     }
 
